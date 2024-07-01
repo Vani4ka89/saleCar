a81e5b1abc6381228cb2d57ae81685b5c961022f
vanko <ivan.tym4ak@gmail.com>
2024-07-01 16:24:21 +0300

    created view repository

--- src/modules/repository/repository.module.ts
+++ src/modules/repository/repository.module.ts
@@ -4,6 +4,7 @@ import { MessageRepository } from './services/message.repository';
 import { UserRepository } from './services/user.repository';
 import { RefreshTokenRepository } from './services/refresh-token.repository';
 import { CurrencyRepository } from './services/currency.repository';
+import { ViewRepository } from './services/view.repository';
 
 const repositories = [
   CarAdRepository,
@@ -11,6 +12,7 @@ const repositories = [
   MessageRepository,
   UserRepository,
   RefreshTokenRepository,
+  ViewRepository,
 ];
 
 @Global()
